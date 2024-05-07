8f5bf2fa8680067cced586f0ab530755f7fb7fd1
ROUGGA <rouggama@gmail.com>
2024-05-07 13:01:51 +0100

    mm

--- checkAvailablePlaces.js
+++ checkAvailablePlaces.js
@@ -1,1 +1,12 @@
-function checkAvailablePlaces()
+function checkAvailablePlaces(id_employee){
+    let id_workplace = getWorkPlace(id_employee);
+    let wantedPlaces = [];
+    wantedPlaces = getWantedPlaces(id_employee);
+    let matchedEmployees= [];
+    matchedEmployees = getMatchedEmployees(id_workplace,wantedPlaces);
+    return matchedEmployees;
+}
+
+function getMatchedEmployees(id_workplace,wantedPlaces){
+    
+}
